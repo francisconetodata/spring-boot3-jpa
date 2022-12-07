7d46a4bfa039fd2ccc8545e414e91a307e29e9ac
Francisco Neto <francisconetodata@gmail.com>
2022-12-06 21:33:43 -0300

    first commit

---
+++ src/main/java/com/aprendendo/treinando/TreinandoApplication.java
@@ -0,0 +1,13 @@
+package com.aprendendo.treinando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinandoApplication.class, args);
+	}
+
+}
